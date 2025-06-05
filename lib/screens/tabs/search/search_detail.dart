144fb6b0bfd10ac8ecb0459edd83bc726efb17d4
Sharren <sharren.brianna@students.mikroskil.ac.id>
2025-06-05 15:51:12 +0700

    search detail hampir siap

--- lib/screens/tabs/search/search_detail.dart
+++ lib/screens/tabs/search/search_detail.dart
@@ -10,175 +10,271 @@ class SearchDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: const Icon(Icons.arrow_back),
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    translate(
-                      context,
-                      'Parking Details',
-                      'Detail Parkir',
-                      '停车详情',
-                    ),
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                ],
-              ),
-            ),
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
-                  mall.image,
-                  width: 400,
-                  height: 300,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    mall.name,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 10,
+                    ),
+
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.arrow_back),
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          translate(
+                            context,
+                            'Parking Details',
+                            'Detail Parkir',
+                            '停车详情',
+                          ),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        mall.image,
+                        width: 400,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
 
-                  Text(
-                    mall.address,
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xFF4D5DFA),
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.directions_car,
-                              color: Color(0xFF4D5DFA),
-                            ),
-                            SizedBox(width: 5),
-                            Text(
-                              mall.spotCount <= 0
-                                  ? 'All Full'
-                                  : mall.spotCount == 1
-                                  ? '${mall.spotCount} Slot'
-                                  : '${mall.spotCount} Slots',
-                              style: TextStyle(
-                                color: Color(0xFF4D5DFA),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xFF4D5DFA),
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.access_time, color: Color(0xFF4D5DFA)),
-                            SizedBox(width: 5),
-                            Text(
-                              '${int.parse(mall.openTime.split(':')[0])} AM - ${int.parse(mall.closeTime.split(':')[0]) - 12} PM',
-                              style: TextStyle(
-                                color: Color(0xFF4D5DFA),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 7,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xFF4D5DFA),
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text(
-                          'Parking',
-                          style: TextStyle(
-                            color: Color(0xFF4D5DFA),
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Description',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Container(
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'It is a very famous shopping center in Medan. Not only a shopping center, but also home to many cafes and trendy dining places. The mall operates from ${mall.openTime} A.M. until ${mall.closeTime} P.M..',
+                          mall.name,
                           style: TextStyle(
-                            color: Color(0xFF908C8C),
-                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
                           ),
                         ),
-                        SizedBox(height: 4),
+
                         Text(
-                          'Read More...',
+                          mall.address,
+                          style: TextStyle(color: Colors.grey, fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 5,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Color(0xFF4D5DFA),
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.directions_car,
+                                    color: Color(0xFF4D5DFA),
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    mall.spotCount <= 0
+                                        ? 'All Full'
+                                        : mall.spotCount == 1
+                                        ? '${mall.spotCount} Slot'
+                                        : '${mall.spotCount} Slots',
+                                    style: TextStyle(
+                                      color: Color(0xFF4D5DFA),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 5,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Color(0xFF4D5DFA),
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.access_time,
+                                    color: Color(0xFF4D5DFA),
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    '${int.parse(mall.openTime.split(':')[0])} AM - ${int.parse(mall.closeTime.split(':')[0]) - 12} PM',
+                                    style: TextStyle(
+                                      color: Color(0xFF4D5DFA),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 7,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Color(0xFF4D5DFA),
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text(
+                                'Parking',
+                                style: TextStyle(
+                                  color: Color(0xFF4D5DFA),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Description',
                           style: TextStyle(
-                            color: Color(0xFF4D5DFA),
-                            fontSize: 16,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'It is a very famous shopping center in Medan. Not only a shopping center, but also home to many cafes and trendy dining places. The mall operates from ${mall.openTime} A.M. until ${mall.closeTime} P.M..',
+                                style: TextStyle(
+                                  color: Color(0xFF908C8C),
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'Read More...',
+                                style: TextStyle(
+                                  color: Color(0xFF4D5DFA),
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color(0xFFEDF4FF)),
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 8,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                            color: Color(0xFFEDF4FF),
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    formatCurrency(nominal: mall.starterPrice),
+                                    style: TextStyle(
+                                      color: Color(0xFF4D5DFA),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                  Text(
+                                    ' / hour',
+                                    style: TextStyle(
+                                      color: Color(0xFF908C8C),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 20,
+                                  horizontal: 30,
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(15),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black12,
+                                      blurRadius: 8,
+                                      offset: Offset(0, 4),
+                                    ),
+                                  ],
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '1st hour',
+                                      style: TextStyle(
+                                        color: Color(0xFF908C8C),
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      formatCurrency(nominal: mall.hourlyPrice),
+                                      style: TextStyle(
+                                        color: Color(0xFF5C5959),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
