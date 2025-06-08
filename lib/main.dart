99270e72536e2a1652a36d3a1d4c7c017c5edfb6
Sharren <sharren.brianna@students.mikroskil.ac.id>
2025-06-08 17:39:17 +0700

    ubah default

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ void main() {
           popupMenuTheme: PopupMenuThemeData(color: Colors.white),
         ),
         debugShowCheckedModeBanner: false,
-        home: LandingScreen(),
+        home: MainApp(),
       ),
     ),
   );
