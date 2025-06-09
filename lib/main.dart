136afdcd9caa18dfc9f0484afe158a7ba5f13347
Howard Javier Leslie <howard@adaremit.co.id>
2025-06-09 10:51:26 +0700

    fix search

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:tugas_front_end_nicolas/provider/parking_lot_provider.dart';
 import 'package:tugas_front_end_nicolas/provider/parking_provider.dart';
 import 'package:tugas_front_end_nicolas/provider/user_provider.dart';
 import 'package:tugas_front_end_nicolas/provider/voucher_provider.dart';
-import 'package:tugas_front_end_nicolas/screens/starting/landing_screen.dart';
 import 'package:tugas_front_end_nicolas/screens/starting/splash_screen.dart';
 import 'package:tugas_front_end_nicolas/screens/starting/stepper.dart';
 
--- lib/screens/tabs/home.dart
+++ lib/screens/tabs/home.dart
@@ -509,6 +509,7 @@ class _HomeState extends State<Home> {
                     ),
                   ],
                 ),
+                SizedBox(height: 10),
               ],
             ),
           ),
