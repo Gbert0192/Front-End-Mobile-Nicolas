6598078dbc55a84a2e9a3e079d77c50f61b4d4ea
EmpthyLL <javileh40@gmail.com>
2025-06-17 00:00:44 +0700

    ya gitu lg

--- lib/screens/tabs/home/parking_lot/booking/booking_time.dart
+++ lib/screens/tabs/home/parking_lot/booking/booking_time.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tugas_front_end_nicolas/components/time_picker.dart';
 import 'package:tugas_front_end_nicolas/model/parking_lot.dart';
+import 'package:tugas_front_end_nicolas/screens/tabs/home/parking_lot/booking.dart';
 import 'package:tugas_front_end_nicolas/utils/index.dart';
-
-class DetailItem {
-  final String label;
-  final String value;
-
-  DetailItem({required this.label, required this.value});
-}
 
 class BookingTime extends StatefulWidget {
   const BookingTime({
@@ -118,59 +112,6 @@ class _BookingTimeState extends State<BookingTime> {
               ),
             ),
           ],
-        ),
-      ],
-    );
-  }
-}
-
-class DataCard extends StatelessWidget {
-  final List<DetailItem> listData;
-  final String title;
-
-  const DataCard({super.key, required this.listData, required this.title});
-  final FontWeight fontWeight = FontWeight.w600;
-  final double fontSize = 13;
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final isSmall = size.height < 700;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: Text(
-            title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-          ),
-        ),
-        Card(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: isSmall ? 25 : 35,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                for (int i = 0; i < listData.length; i++) ...[
-                  buildDetailRow(
-                    label: listData[i].label,
-                    value: listData[i].value,
-                    fontSize: fontSize,
-                    fontWeight: fontWeight,
-                  ),
-                  if (i != listData.length - 1) const SizedBox(height: 10),
-                ],
-              ],
-            ),
-          ),
         ),
       ],
     );
