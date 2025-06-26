aa1992f858991625dfc17aee4f5f739f2d195159
Sharren <sharren.brianna@students.mikroskil.ac.id>
2025-06-26 13:42:07 +0700

    receipt

--- lib/screens/tabs/park&book/receipt copy.dart
+++ lib/screens/tabs/park&book/receipt copy.dart
@@ -2,40 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:tugas_front_end_nicolas/components/button.dart';
 import 'package:tugas_front_end_nicolas/components/detail_component.dart';
 import 'package:tugas_front_end_nicolas/utils/index.dart';
-import 'package:tugas_front_end_nicolas/model/parking.dart';
 
-class Receipt extends StatefulWidget {
-  final Parking parking;
+class ReceiptT extends StatefulWidget {
+  // final Parking parking;
   final String spotTypeEn;
   final String spotTypeId;
   final String spotTypeCn;
-  final String? slot;
 
-  const Receipt({
+  const ReceiptT({
     super.key,
-    required this.parking,
+    // required this.parking,
     required this.spotTypeEn,
     required this.spotTypeId,
     required this.spotTypeCn,
-    required this.slot,
   });
 
   @override
-  State<Receipt> createState() => _ReceiptState();
+  State<ReceiptT> createState() => _ReceiptTState();
 }
 
-class _ReceiptState extends State<Receipt> {
+class _ReceiptTState extends State<ReceiptT> {
   @override
   Widget build(BuildContext context) {
-    final parts = widget.slot!.split("-");
     final List<DetailItem> bookInfo = [
       DetailItem(
         label: translate(context, 'Parking Area', 'Area Parkir', '停车场'),
-        value: widget.parking.lot.name,
+        value: 'widget.parking.lot.name',
       ),
       DetailItem(
         label: translate(context, 'Address', 'Alamat', '地址'),
-        value: widget.parking.lot.address,
+        value: 'widget.parking.lot.address',
       ),
       DetailItem(
         label: translate(
@@ -44,58 +40,28 @@ class _ReceiptState extends State<Receipt> {
           widget.spotTypeId,
           widget.spotTypeCn,
         ),
-        value: "${formatFloorLabel(parts[0])} (${parts[1]})",
+        value: "formatFloorLabel(parts[0])} (parts[1]})",
       ),
       DetailItem(
         label: translate(context, 'Duration', 'Durasi', '持续时间'),
-        value: '${widget.parking.calculateHour()} hours',
+        value: 'widget.parking.calculateHour()} hours',
       ),
       DetailItem(
         label: translate(context, 'Check-in Time', 'Waktu Masuk', '签到时间'),
-        value:
-            widget.parking.checkoutTime != null
-                ? formatDateTime(widget.parking.checkoutTime!)
-                : '-',
+        value: 'widget.parking.checkoutTime',
       ),
 
       DetailItem(
         label: translate(context, 'Check-out Time', 'Waktu Keluar', '签出时间'),
-        value:
-            widget.parking.checkinTime != null
-                ? formatDateTime(widget.parking.checkinTime!)
-                : '-',
+        value: 'widget.parking.checkinTime',
       ),
     ];
 
     final List<DetailItem> prices = [
-      DetailItem(
-        label: "Amount",
-        value: formatCurrency(
-          nominal: widget.parking.amount ?? 0,
-          decimalPlace: 0,
-        ),
-      ),
-      DetailItem(
-        label: "Taxes (11%)",
-        value: formatCurrency(
-          nominal: widget.parking.tax ?? 0,
-          decimalPlace: 0,
-        ),
-      ),
-      DetailItem(
-        label: "Service Fee",
-        value: formatCurrency(
-          nominal: widget.parking.service ?? 0,
-          decimalPlace: 0,
-        ),
-      ),
-      DetailItem(
-        label: "Total",
-        value: formatCurrency(
-          nominal: widget.parking.total ?? 0,
-          decimalPlace: 0,
-        ),
-      ),
+      DetailItem(label: "Amount", value: 'widget.parking.amount ?? 0'),
+      DetailItem(label: "Taxes (11%)", value: 'widget.parking.tax ?? 0'),
+      DetailItem(label: "Service Fee", value: 'widget.parking.service ?? 0'),
+      DetailItem(label: "Total", value: 'widget.parking.total ?? 0'),
     ];
 
     return Scaffold(
